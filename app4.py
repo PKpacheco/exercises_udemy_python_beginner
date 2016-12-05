be97797afd6fe4ebd786b27898dd8f6eec412b8d
Paola Katherine Cardoso Pacheco <paolakatherine@MacBook-Air-de-Paola.local>
2016-12-05 18:55:06 -0300

    exercise 4 + json+ requests

--- app4.py
+++ app4.py
@@ -1,17 +1,7 @@
+import requests
+
 __author__ = "pkpacheco"
 
+request = requests.get("https://www.google.com")
 
-def ask_age():
-    age = input("Enter your age: ")
-    return int(age)
-
-
-def calculate_seconds_from_years(age_years):
-    return age_years * 365 * 24 * 3600
-
-
-def prompt_user_and_calculate_age():
-    age = ask_age()
-    seconds_lived = calculate_seconds_from_years(age)
-    print ("You have lived for {} seconds.".format(seconds_lived))
-prompt_user_and_calculate_age()
+print(request.content)
