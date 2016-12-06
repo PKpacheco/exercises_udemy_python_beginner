66be27e091fc0327179ceb062901a1d9f83eec2b
Paola Katherine Cardoso Pacheco <paolakatherine@MacBook-Air-de-Paola.local>
2016-12-06 17:50:55 -0300

    gitingre

---
+++ part_2/app2.py
@@ -0,0 +1,7 @@
+from models.post2 import Post
+
+post = Post("Post1 title", "Post1 content", "Post1 author")
+post2 = Post("Post2 title", "Post2 content", "Post2 author")
+
+print(post.content)
+print(post2.content)
---
+++ part_2/models/post2.py
@@ -0,0 +1,9 @@
+__author__ = "pkpacheco"
+
+
+class Post(object):
+
+    def __init__(self, title, content, author):
+        self.title = title
+        self.content = content
+        self.author = author
