ad1d3fe666f53b706fe72c4e61bbfcb0938c6ae8
Paola Katherine Cardoso Pacheco <paolakatherine@MacBook-Air-de-Paola.local>
2016-12-07 08:36:12 -0300

    part 2'

---
+++ part_2/models/post3.py
@@ -0,0 +1,26 @@
+__author__ = "pkpacheco"
+
+
+class Post(object):
+
+    def __init__(self, blog_id, title, content, author, date, id):
+        self.blog_id = blog_id
+        self.title = title
+        self.content = content
+        self.author = author
+        self.created_date = date
+        self.id = id
+
+    def save_to_mongo(self):
+        Database.insert(collection='posts',
+                        data=self.json())
+
+    def json(self):
+        return {
+            'id': self.id,
+            'blog_id': self.blog_id
+            'author': self.author
+            'content': self.content
+            'title': self.title
+            'created_date': self.created_date
+        }
