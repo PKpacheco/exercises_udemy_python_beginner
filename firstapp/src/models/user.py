f5c36a2b8fdd93e5de3bdf313424b06a0a689f6d
Pk Pacheco <pkcpweb@gmail.com>
2017-02-16 12:19:34 -0300

    methods user

--- firstapp/src/models/user.py
+++ firstapp/src/models/user.py
@@ -1,21 +1,53 @@
+import uuid
+from src.commom.database import Database
+
+
 class User(object):
-    def __init__(self):
-        pass
+    def __init__(self, email, password, _id=None):
+        self.email = email
+        self.password = password
+        self._id = uuid.uuid4().hex if _id is None else _id
 
-    def fromget_by_email(self):
-        pass
+    @classmethod
+    def get_by_email(cls, email):
+        data = Database.find_one("users", {"email": email})
+        if data is not None:
+            return cls(**data)
 
-    def get_by_id(self):
-        pass
+    @classmethod
+    def get_by_id(cls, _id):
+        data = Database.find_one("users", {"_id": _id})
+        if data is not None:
+            return cls(**data)
 
-    def login_valid(self):
-        pass
+    @staticmethod
+    def login_valid(email, password):
+        user = User.get_by_email(email)
+        if user is not None:
+            # check the password
+            return user.password == password
+        return False
 
-    def register(self):
-        pass
+    @staticmethod
+    def register(cls, email, password):
+        user = cls.get_by_email(email)
+        if user is None:
+            # user doesn't not exist, so we can create it
+            new_user = cls(email, password)
+            new_user.save_to_mongo()
+            return True
+        else:
+            # user exists
+            return False
 
     def login(self):
         pass
 
     def get_blogs(self):
+        pass
+
+    def json(self):
+        pass
+
+    def save_to_mongo(self):
         pass
